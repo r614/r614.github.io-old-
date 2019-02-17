f3eb66f40385c280e6b16c908686ca8a6f06504f
Roshan Pawar <steradian614@gmail.com>
2019-02-16 21:15:20 -0800

    Added buttons.

--- webapp/src/components/buttons.js
+++ webapp/src/components/buttons.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
+
 const Buttons = () => (
 
     <section class='buttonSection'>
         <div class='buttons'>
-            Github Projects Resume
+            <button class='hvr-sweep-to-left'> Github </button>
+            <button class='hvr-sweep-to-left'> Projects </button>
+            <button class='hvr-sweep-to-left'> Resume </button>
         </div>
-
     </section>
-
 
 );
 
